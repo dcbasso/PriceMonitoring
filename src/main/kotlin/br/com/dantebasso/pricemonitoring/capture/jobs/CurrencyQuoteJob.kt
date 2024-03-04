d030d7dd15bf62fb0089357f0e87ef79f8f32a20
dbasso <dbasso@expediagroup.com>
2024-03-03 22:01:08 -0300

    Added schdedule config to run every day at 8am, but its configurable

--- src/main/kotlin/br/com/dantebasso/pricemonitoring/capture/jobs/CurrencyQuoteJob.kt
+++ src/main/kotlin/br/com/dantebasso/pricemonitoring/capture/jobs/CurrencyQuoteJob.kt
@@ -1,19 +1,23 @@
 package br.com.dantebasso.pricemonitoring.capture.jobs
 
 import br.com.dantebasso.pricemonitoring.capture.CurrencyQuoteCaptureService
+import org.quartz.DisallowConcurrentExecution
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
+@DisallowConcurrentExecution
 class CurrencyQuoteJob @Autowired constructor(
     private val currencyQuoteCaptureService: CurrencyQuoteCaptureService
 ) : Job {
 
     private val logger = LoggerFactory.getLogger(CurrencyQuoteJob::class.java)
 
+    @Scheduled(cron = "capture.cron.scheduled")
     override fun execute(context: JobExecutionContext) {
         logger.info("Executing CurrencyQuoteJob...")
         currencyQuoteCaptureService.capture()
