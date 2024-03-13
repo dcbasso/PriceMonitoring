2217a10cc7d80b6b5e6805e79cba542f6c993b55
dcbasso <dcbasso@gmail.com>
2024-03-13 16:37:44 -0300

    fix unit test

--- src/test/kotlin/br/com/dantebasso/pricemonitoring/capture/VisaoVipCaptureServiceUnitTest.kt
+++ src/test/kotlin/br/com/dantebasso/pricemonitoring/capture/VisaoVipCaptureServiceUnitTest.kt
@@ -2,6 +2,7 @@ package br.com.dantebasso.pricemonitoring.capture
 
 import br.com.dantebasso.pricemonitoring.capture.processor.VisaoVipLineProcessor
 import br.com.dantebasso.pricemonitoring.service.JobCaptureLogService
+import br.com.dantebasso.pricemonitoring.service.mail.EmailServiceSender
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
@@ -17,15 +18,20 @@ class VisaoVipCaptureServiceUnitTest {
     @RelaxedMockK
     lateinit var jobCaptureLogServiceMockk: JobCaptureLogService
 
+    @RelaxedMockK
+    lateinit var emailServiceSenderMockk: EmailServiceSender
+
     @Test
     fun requestToWebsiteShouldTryProcessTheDownloadedFile() {
         val toTest = VisaoVipCaptureService(
             processor = processorMockk,
-            jobCaptureLogService = jobCaptureLogServiceMockk
+            jobCaptureLogService = jobCaptureLogServiceMockk,
+            emailServiceSender = emailServiceSenderMockk
         )
         toTest.capture()
         verify(atLeast = 1) { processorMockk.processLine(any()) }
         verify(exactly = 1) { jobCaptureLogServiceMockk.save(any()) }
+        verify(exactly = 1) { emailServiceSenderMockk.sendNotificationEmailWhenSuccessfullyFinishedTheJobProcess(any()) }
     }
 
 }
