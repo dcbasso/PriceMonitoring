929343d002a02fdb532f3fb0759bbbb09c764d51
Dante Basso <dcbasso@gmail.com>
2024-03-13 12:09:16 -0300

    fix issue

--- src/main/kotlin/br/com/dantebasso/pricemonitoring/service/mail/EmailServiceSender.kt
+++ src/main/kotlin/br/com/dantebasso/pricemonitoring/service/mail/EmailServiceSender.kt
@@ -1,10 +1,12 @@
 package br.com.dantebasso.pricemonitoring.service.mail
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
+import java.util.*
 
 
 @Service
@@ -14,16 +16,26 @@ class EmailServiceSender @Autowired constructor(
     private val emailSender: JavaMailSender
 ) {
 
+    private val logger = LoggerFactory.getLogger(EmailServiceSender::class.java)
+
     fun sendEmail() {
 
     }
 
     fun sendNotificationEmail() {
-        val message = SimpleMailMessage()
-        message.setTo(emailDestiny)
-        message.setSubject("teste")
-        message.setText("Processado com sucesso")
-        emailSender.send(message)
+        try {
+            val message = SimpleMailMessage()
+            message.setTo("dcbasso@gmail.com")
+            message.subject = "teste"
+            message.text = "Processado com sucesso"
+            message.from = "notification@dantebasso.com.br"
+            message.replyTo = "notification@dantebasso.com.br"
+            message.sentDate = Date()
+//            message.to = arrayOf("dcbasso@gmail.com")
+            emailSender.send(message)
+        } catch (e: Exception) {
+           logger.error(e.message)
+        }
     }
 
 
